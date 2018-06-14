39a5b80623b770c6090b1cda40ecde98c2d7c41d
Liaz kamper <liaz@stratoscale.com>
2018-06-14 19:39:32 +0300

    add LC_ALL to env

--- terraform/wordpress-app/wpdeploy_unused.sh
+++ terraform/wordpress-app/wpdeploy_unused.sh
@@ -4,14 +4,11 @@ export DBHOST=${db_ip}
 export DBUSER=${db_user}
 export DBPASSWORD=${db_password}
 export DBNAME=${db_name}
-
-
-echo "@@@@ before update image"
+export LC_ALL="en_US.UTF-8"
 
 # update image
 apt-get update -y
 apt-get upgrade -y
-echo "@@@@ after update image"
 
 # install docker
 apt-get install -y docker.io
