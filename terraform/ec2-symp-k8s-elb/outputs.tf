107d11a82bf23846777a5f52e8105f7f19a7db39
Liaz kamper <liaz@stratoscale.com>
2019-03-26 18:40:22 +0200

    k8s symp: remove nginx output from wordpress solution

--- terraform/ec2-symp-k8s-elb/outputs.tf
+++ terraform/ec2-symp-k8s-elb/outputs.tf
@@ -2,10 +2,6 @@ output "lb_eip" {
   value = "${aws_alb.alb.dns_name}"
 }
 
-output "nginx_app_endpoint" {
-  value = "${aws_alb.alb.dns_name}:${aws_alb_listener.list.port}"
-}
-
 output "wordpres_app_endpoint" {
   value = "${aws_alb.alb.dns_name}:${aws_alb_listener.wp_list.port}"
 }
