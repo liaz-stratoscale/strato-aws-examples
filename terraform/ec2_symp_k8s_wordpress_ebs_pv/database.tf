44870eb72f9866582227884f0b45716d5784c4ef
Liaz kamper <liaz@stratoscale.com>
2019-06-25 13:48:36 +0300

    Terraform Symp K8S - adjust to terraform 0.12

--- terraform/ec2_symp_k8s_wordpress_ebs_pv/database.tf
+++ terraform/ec2_symp_k8s_wordpress_ebs_pv/database.tf
@@ -3,31 +3,36 @@
 #make db subnet group 
 resource "aws_db_subnet_group" "dbsubnet" {
   name       = "main"
-  subnet_ids = ["${aws_subnet.db_subnet.id}"]
+  subnet_ids = [aws_subnet.db_subnet.id]
 }
 
 #provision the database
 resource "aws_db_instance" "wpdb" {
-  identifier = "k8s-wpdb"
-  instance_class = "db.m1.large"
-  allocated_storage = 20
-  engine = "mysql"
-  name = "k8s_wordpress_db"
-  password = "${var.db_password}"
-  username = "${var.db_user}"
-  engine_version = "5.7.00"
-  skip_final_snapshot = true
-  db_subnet_group_name = "${aws_db_subnet_group.dbsubnet.name}"
-  vpc_security_group_ids = ["${aws_security_group.db.id}"]
+  identifier             = "k8s-wpdb"
+  instance_class         = "db.m1.large"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  name                   = "k8s_wordpress_db"
+  password               = var.db_password
+  username               = var.db_user
+  engine_version         = "5.7.00"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.dbsubnet.name
+  vpc_security_group_ids = [aws_security_group.db.id]
+
   # Workaround for Symphony 
   lifecycle {
-    ignore_changes = ["engine", "auto_minor_version_upgrade", "vpc_security_group_ids"]
+    ignore_changes = [
+      engine,
+      auto_minor_version_upgrade,
+      vpc_security_group_ids,
+    ]
   }
 }
 
 resource "aws_security_group" "db" {
-  name = "db-secgroup"
-  vpc_id = "${aws_vpc.app_vpc.id}"
+  name   = "db-secgroup"
+  vpc_id = aws_vpc.app_vpc.id
 
   # ssh access from anywhere
   ingress {
@@ -36,11 +41,12 @@ resource "aws_security_group" "db" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
--- terraform/ec2_symp_k8s_wordpress_ebs_pv/variables.tf
+++ terraform/ec2_symp_k8s_wordpress_ebs_pv/variables.tf
@@ -1,12 +1,23 @@
 # AWS API credentials
-variable "secret_key" {}
-variable "access_key" {}
-variable "symphony_ip" {}
+variable "secret_key" {
+}
+
+variable "access_key" {
+}
+
+variable "symphony_ip" {
+}
 
 # Symphony API credentials
-variable "symp_domain" {}
-variable "symp_user" {}
-variable "symp_password" {}
+variable "symp_domain" {
+}
+
+variable "symp_user" {
+}
+
+variable "symp_password" {
+}
+
 variable "symp_project" {
   description = "Project Name"
 }
@@ -15,31 +26,35 @@ variable "symp_project" {
 variable "k8s_name" {
   description = "Kubernetres cluster name. Must be unique in this project"
 }
+
 variable "k8s_eng" {
   description = "Kubernetes engine version"
-  default = "1.13"
+  default     = "1.13"
 }
+
 variable "k8s_size" {
   description = "Size of data disk of each node"
-  default = "50"
+  default     = "50"
 }
+
 variable "k8s_count" {
   description = "Initial cluster node count"
-  default = "2"
+  default     = "2"
 }
+
 variable "k8s_type" {
   description = "Instance type of each node"
-  default = "t2.large"
+  default     = "t2.large"
 }
 
 variable "k8s_configfile_path" {
   description = "Path to place the Kubernetes config file"
-  default = "~/.kube/config"
+  default     = "~/.kube/config"
 }
 
 variable "k8s_private_registry" {
   description = "Address and port of a private registry to be added. Only insecure. e.g. 1.2.3.4:5000"
-  default = ""
+  default     = ""
 }
 
 # Application variables
@@ -48,12 +63,13 @@ variable "wordpress_image" {
 }
 
 variable "wordpress_port" {
-  default = 8080
+  default     = 8080
   description = "The Wordpress service port"
 }
+
 variable "dns_list" {
-  type = "list"
-  default = ["8.8.4.4", "8.8.8.8"]
+  type        = list(string)
+  default     = ["8.8.4.4", "8.8.8.8"]
   description = "DNS list to be attached to the VPC subnets"
 }
 
@@ -65,3 +81,4 @@ variable "db_user" {
 variable "db_password" {
   description = "Password for the DB"
 }
+
