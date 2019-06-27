0bc121ec51f653cdc754e2287911be2ed0e49613
Liaz kamper <liaz@stratoscale.com>
2019-06-27 16:37:50 +0300

    Terraform EC2 instances - adjust to terraform 0.12

--- terraform/ec2-instance/aws-provider.tf
+++ terraform/ec2-instance/aws-provider.tf
@@ -1,21 +1,17 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
 
   endpoints {
     ec2 = "https://${var.symphony_ip}/api/v2/aws/ec2"
-    elb = "https://${var.symphony_ip}/api/v2/aws/elb"
-    rds = "https://${var.symphony_ip}/api/v2/aws/rds"
   }
 
-  insecure = "true"
-  skip_metadata_api_check = true
+  insecure                    = true
+  skip_metadata_api_check     = true
   skip_credentials_validation = true
-  skip_requesting_account_id = true
-
-  # Pinning AWS plugin version
-  version = "=1.31.0"
+  skip_requesting_account_id  = true
 
   # No importance for this value currently
   region = "us-east-1"
 }
+
--- terraform/ec2-instance/simple-ec2.tf
+++ terraform/ec2-instance/simple-ec2.tf
@@ -1,11 +1,13 @@
 # Create instances, and name them according to count
 resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_image}"
+  ami = var.ami_image
 
-    tags{
-        Name="instance${count.index}"
-    }
-    # Can use any aws instance type supported by symphony
-    instance_type = "${var.instance_type}"
-    count="${var.instance_number}"
+  tags = {
+    Name = "instance${count.index}"
+  }
+
+  # Can use any aws instance type supported by symphony
+  instance_type = var.instance_type
+  count         = var.instance_number
 }
+
