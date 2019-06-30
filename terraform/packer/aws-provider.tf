01c64af100855959f58aff6ee259a572f18d982f
Liaz kamper <liaz@stratoscale.com>
2019-06-30 12:19:28 +0300

    Terraform and Packer - adjust to terraform 0.12 and Packer 1.4.2

--- terraform/packer/aws-provider.tf
+++ terraform/packer/aws-provider.tf
@@ -1,20 +1,17 @@
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
 
+  endpoints {
+    ec2 = "https://${var.symphony_ip}/api/v2/aws/ec2"
+  }
 
-    endpoints {
-        ec2 = "https://${var.symphony_ip}/api/v2/aws/ec2"
-    }
+  insecure                    = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
 
-    insecure = "true"
-    skip_metadata_api_check = true
-    skip_credentials_validation = true
-    skip_requesting_account_id = true
-
-    # No importance for this value currently
-    region = "us-east-1"
-
-    # Pin AWS plugin to 1.31.0
-    version = "=1.31.0"
+  # No importance for this value currently
+  region = "us-east-1"
 }
+
